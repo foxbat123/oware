e2c5f72c26c9ef7bd4a7c704877f13cdae2c4669
Matthew <MDPitcairn@gmail.com>
2019-03-14 14:43:24 +0200

    Adding comments to explain initial functions

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -7,12 +7,15 @@ type StartingPosition =
 let getSeeds n board = failwith "Not implemented"
 
 let useHouse n board = failwith "Not implemented"
+    //Removes and sows the seeds from house n 
 
 let start position = failwith "Not implemented"
+    //Sets up the board tuples for the start of the game
 
 let score board = failwith "Not implemented"
 
 let gameState board = failwith "Not implemented"
+    //Determines who's turn it is
 
 [<EntryPoint>]
 let main _ =
