3235610eb90c59ebe808e67b408b13668376d624
Matthew <MDPitcairn@gmail.com>
2019-03-21 11:02:19 +0200

    Added seeding function, not yet operational

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -6,33 +6,21 @@ open System
 open System
 open System
 
-type board = {
-    A : int
-    B : int 
-    C : int 
-    D : int
-    E : int
-    F : int
-    a : int
-    b : int
-    c : int
-    d : int
-    e : int
-    f : int
+type board = { //Using records to represent the houses, the name of field indicates the number of the house and the 
+               //assoicated integer value is the number of seeds in that house
+    A : int; B : int; C : int; D : int; E : int; F : int; a : int; b : int; c : int; d : int; e : int; f : int
 }
 
 //type player 
 
 (*Prototype:
-        Board as a represenation is a tuple of type house (contains the name of the house and the 
+        Board as a represenation is a record (contains the name of the house and the 
         amount of seeds in that house)
 
         Seperate seeding and capture function
             Both functions need to be recursive 
 
         Can't capture from own house
-
-
 
         Where to store the score?
 
@@ -70,10 +58,8 @@ let getSeeds n board =
     | _ -> failwith "Invalid house"
     //Accepts a house number and a ​board​, and returns the number of  seeds in the specified house. 
 
-let useHouse n board = failwith "Not implemented"
-    //Removes and sows the seeds from house n counterclockwise.
-
-let start position = {A = 4; B = 4; C = 4; D = 4; E = 4; F = 4; a = 4; b = 4; c = 4; d = 4; e = 4; f = 4}
+let start position = 
+    {A = 4; B = 4; C = 4; D = 4; E = 4; F = 4; a = 4; b = 4; c = 4; d = 4; e = 4; f = 4}
     //Sets up the board tuples for the start of the game. Output is the board (?) Position determines which player starts the game
 
 let score board = failwith "Not implemented"
@@ -82,11 +68,49 @@ let score board = failwith "Not implemented"
 let gameState board = failwith "Not implemented"
     //Determines whose turn it is.
 
-let seed board start = failwith "Not implemented"
-    //performs the seed operation
+let useHouse start board =
+    //Removes and sows the seeds from house start counterclockwise.
+    let numSeeds = getSeeds start board //Grabs the amount of seeds that are picked up
+    let board = changeSeeds board start 0 //Set the starting house to 0 seeds
 
+    let rec seeding board house numseeds startHouse = //Recursive function to handle
+        let board = changeSeeds board house ((getSeeds board house)+1) //Adds one seed to house
+        let house = match house = 12 with //Handles the wrapping of the houses
+                    | true -> 1
+                    | false -> house
+        match numseeds > 0 with
+        | true -> match house = startHouse - 1 with //Skips the house we intially took from
+                  | true -> seeding board (house+2) (numseeds-1) startHouse
+                  | false -> seeding board (house+1) (numseeds-1) startHouse
+        | false -> board //Not sure about what to do with the base case
+
+    seeding board (start + 1) numSeeds start
+        
 let capture board start = failwith "Not implemented"
 
+let changeSeeds board n seeds =
+    match n with 
+    | 1 -> {board with A = seeds}
+    | 2 -> {board with B = seeds}
+    | 3 -> {board with C = seeds}
+    | 4 -> {board with D = seeds}
+    | 5 -> {board with E = seeds}
+    | 6 -> {board with F = seeds}
+    | 7 -> {board with a = seeds}
+    | 8 -> {board with b = seeds}
+    | 9 -> {board with c = seeds}
+    | 10 -> {board with d = seeds}
+    | 11 -> {board with e = seeds}
+    | 12 -> {board with f = seeds}
+    | _ -> failwith "Invalid house"
+
+let drawBoard board = 
+    let string = 
+          "|============{N}===========|\n"
+        + "|==%i==%i==%i==%i==%i==%i==|\n" board.a, board.b, board.c, board.d, board.e, board.f
+        + "|--------------------------|\n"
+        + "|==%i==%i==%i==%i==%i==%i==|\n" board.A, board.B, board.C, board.D, board.E, board.F
+        + "|============{S}===========|\n"
 
 [<EntryPoint>]
 let main _ =
